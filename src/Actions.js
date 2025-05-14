fa43e52f854c5aed3678671b610c2219b15ada84
parvindar <parvindarsingh43958@gmail.com>
2025-05-15 00:40:38 +0530

    update Actions.js

--- src/Actions.js
+++ src/Actions.js
@@ -1,10 +1,9 @@
-const ACTIONS = {
-    JOIN:'join',
-    JOINED:'joined',
-    DISCONNECTED:'disconnected',
-    CODE_CHANGE:'code-change',
-    SYNC_CODE:'sync-code',
-    LEAVE:'leave',
-}
-
-module.exports = {ACTIONS};
+module.exports = {
+  ACTIONS: {
+    JOIN: 'join',
+    JOINED: 'joined',
+    CODE_CHANGE: 'code-change',
+    SYNC_CODE: 'sync-code',
+    DISCONNECTED: 'disconnected'
+  }
+};
