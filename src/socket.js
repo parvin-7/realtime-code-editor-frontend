c30512cae521828b0820dd10519c568bed1cd8ff
parvindar <parvindarsingh43958@gmail.com>
2025-05-16 14:24:56 +0530

    update socket.js

--- src/socket.js
+++ src/socket.js
@@ -8,5 +8,11 @@ export const initSocket = async () => {
     reconnectionAttempts: 5,
     reconnectionDelay: 1000,
   });
+  socket.on('connect', () => {
+    console.log('Socket connected on client:', socket.id);
+  });
+  socket.on('connect_error', (err) => {
+    console.error('Socket connection error:', err);
+  });
   return socket;
 };
